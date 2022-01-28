0cf0af63ae8c11c662bc706532e1922550c3def3
Nadia <nadia.mohamed89@gmail.com>
2022-01-28 10:49:03 +0100

    Update format

--- resources/functions.php
+++ resources/functions.php
@@ -93,9 +93,9 @@ Container::getInstance()
 
 function elearning_theme_files() {
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    }
+}
     
-    add_action('wp_enqueue_scripts', 'elearning_theme_files');
+add_action('wp_enqueue_scripts', 'elearning_theme_files');
 
 // New Role - Student
 add_role( 'student_role', __( 'Student' ), array( 'read' => true ) ); 
